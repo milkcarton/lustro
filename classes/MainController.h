7d0059579dc051d7e419182751bc818bba35b8e4
Jelle Vandebeeck <jelle@fousa.be>
2008-04-20 19:24:39 +0000

    - Connect checkboxes to user defaults using bindings - Set the method when export button is pressed

---
+++ classes/MainController.h
@@ -0,0 +1,16 @@
+//
+//  MainController.h
+//  lustro
+//
+//  Created by Jelle Vandebeeck & Simon Schoeters on 20/04/08.
+//  Copyright 2008 eggnog. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MainController : NSObject {
+}
+
+- (IBAction)export:(id)sender;
+
+@end
---
+++ classes/MainController.m
@@ -0,0 +1,18 @@
+//
+//  MainController.m
+//  lustro
+//
+//  Created by Jelle Vandebeeck & Simon Schoeters on 20/04/08.
+//  Copyright 2008 eggnog. All rights reserved.
+//
+
+#import "MainController.h"
+
+@implementation MainController
+//
+// Called when the export button is pressed.
+//
+- (IBAction)export:(id)sender
+{
+}
+@end
